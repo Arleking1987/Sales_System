a97e618c4682855cf7f7eec2115587c35026edba
Jona <nathan0x0@gmail.com>
2022-09-08 08:11:15 -0500

    Se inicia el proyecto hasta la actualizacion de usuarios

---
+++ Sales_sistem/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sales_sistem.Areas.Users.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales_sistem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {           
+        }
+
+        public DbSet<TUsers> TUsers { get; set; }
+    }
+}
